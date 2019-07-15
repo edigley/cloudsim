01e09bae81e084901afa105788a8d42449ef0076
Edigley Fraga <edigley@gmail.com>
2019-07-16 00:49:46 +0200

    Add support to new generation instances

---
+++ input-files/createPlaypen.sh
@@ -0,0 +1,6 @@
+mkdir playpen/
+cp -r target/dependency-jars/ playpen/
+cp -r input-files playpen/
+cp /home/edigley/.m2/repository/com/edigley/cloudsim/0.0.1-SNAPSHOT/cloudsim-0.0.1-SNAPSHOT.jar playpen/
+
+exit 0;
--- src/main/java/com/edigley/cloudsim/ui/Main.java
+++ src/main/java/com/edigley/cloudsim/ui/Main.java
@@ -2,12 +2,10 @@ package com.edigley.cloudsim.ui;
 
 public class Main {
 
-	/*
-	 */
 	public static void main(String[] args) throws Exception {
 
-		int spotLimit = 1; // 1 | 2 | 10 | 20 | 64 | 128 | 256
-		String instanceType = "m1.small"; // c1.xlarge | c1.medium | m2.4xlarge | m2.2xlarge | m2.xlarge | m1.xlarge | m1.large | m1.small  
+		int spotLimit = 64; // 2| 4 | 8 | 16 | 32 | 64 | 128
+		String instanceType = "c5.large"; // c5.18xlarge | c5.9xlarge | c5.4xlarge | c5.2xlarge | c5.xlarge | c5.large | c1.xlarge | c1.medium | m2.4xlarge | m2.2xlarge | m2.xlarge | m1.xlarge | m1.large | m1.small  
 
 		String wkl = "input-files/jonquera_single_core_8_workers_trace_mwa.txt";
 		String spp = "input-files/us-east-1.linux." + instanceType + ".csv";
@@ -16,8 +14,6 @@ public class Main {
 		String spi = "input-files/jonquera_ec2_instances.txt";
 		String output = "spot-trace-persistent_output.txt";
 
-		//int[] spotLimits = new int[] {1, 2, 10, 20, 64, 128, 256}; 
-		
 		String cmdPattern = "spotsim.jar -spot -l %s -bid max -w %s -av %s -pd %s -md %s -ait %s -o %s";
 		String spotsimCMD = String.format(cmdPattern, spotLimit, wkl, spp, isd, md, spi, output);
 		args = spotsimCMD.split("\\s+");
